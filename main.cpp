10eb89290ab45a9bc797b02fcef9ca37cc0cc92d
Carlos R. Arias <arias@spu.edu>
2018-11-16 09:11:30 -0800

    Ready for exercise

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "person.h"
+#include <iostream>
+#include <sstream>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::stringstream;
+
+
+int main(int argc, char* argv[]){
+	stringstream inputBuffer;
+	inputBuffer.str(
+			"John 15\n"
+			"Ann 19\n"
+			"Mary 21\n"
+			"Elizabeth 25\n"
+			"Peter 41"
+	);
+
+	// Create an array of Persons
+
+	// Use inputBuffer to fill the array of Persons
+
+	// Output to screen the array of person
+	// first output using ToString
+	// second output using Write
+
+	// Do you need to do something else before finishing?
+
+	return 0;
+}
